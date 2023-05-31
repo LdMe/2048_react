c75df8ca1356a3f86016b85dfc433e1a223d4bc5
danel-torre-ubuntu <dlafuente003@gmail.com>
2023-05-31 10:43:56 +0200

    extra functions removed

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -5,257 +5,24 @@ import Tile from "../Tile/Tile";
 import "./Board.scss";
 
 const Board = ({action=null}) => {
+
+    // estado board que contiene los valores de las casillas
     const [board, setboard] = useState([
     [0, 0, 0, 0],
     [0, 0, 0, 0],
     [0, 0, 0, 0],
     [0, 0, 0, 0],
     ]);
-    const emptyMovements = [
-        [[0,0],[0,0],[0,0],[0,0]],
-        [[0,0],[0,0],[0,0],[0,0]],
-        [[0,0],[0,0],[0,0],[0,0]],
-        [[0,0],[0,0],[0,0],[0,0]],
-    ];
-    const [movements, setmovements] = useState(emptyMovements);
-    const [isMoving, setisMoving] = useState(false);
-    const boardRef = useRef(board);
-    const movementsRef = useRef(movements);
-    const movingRef = useRef(isMoving);
-
-    const setBoard = (data) => {
-        boardRef.current = data;
-        setboard(data);
-        setMovements(emptyMovements);
-    };
-
-    const setMovements = (data) => {
-        movementsRef.current = data;
-        setmovements(data);
-    };
-
-    const setIsMoving = (data) => {
-        movingRef.current = data;
-        setisMoving(data);
-    };
-    useEffect(() => {
-        console.log("action",action);
-        if (action.action === "reset") {
-            resetBoard();
-        }
-        if (["left", "right", "up", "down"].includes(action.action)){
-            move(action.action);
-        }
-    }, [action]);
     
-    const copyBoard = (board) => {
-        const newBoard = [];
-        board.forEach(row => {
-            const newRow = [];
-            row.forEach(tile => {
-                newRow.push(tile);
-            });
-            newBoard.push(newRow);
-        });
-        return newBoard;
-    };
-
-    const resetBoard = () => {
-        setBoard(createRandomTile(createRandomTile(createEmptyBoard())));
-    };
-
-    const createEmptyBoard = () => {
-        const board =Array(4).fill(0).map(() => Array(4).fill(0));
-        return board;
-    };
-
-    const move = (direction) => {
-        console.log("move",direction);
-        if (movingRef.current) {
-            return;
-        }
-        setIsMoving(true);
-        const possibleMovements = {
-            left: moveLeft,
-            right: moveRight,
-            up: moveUp,
-            down: moveDown,
-        };
-        let newBoard = copyBoard(boardRef.current);
-        let newMovements = copyBoard(emptyMovements);
-        [newBoard,newMovements] = possibleMovements[direction](newBoard,newMovements);
-        setMovements(newMovements);
-        newBoard = createRandomTile(newBoard);
-        setTimeout(() => {
-            setBoard(newBoard);
-            setIsMoving(false);
-        }, 300);
-    };
-
-    const invertHorizontal = (board,movements) => {
-        const newBoard = invertHorizontalArray(board)
-        const newMovements = invertHorizontalArray(movements)
-        return [newBoard,newMovements];
-    };
-    
-    const invertVertical = (board,movements) => {
-        const newBoard = invertVerticalArray(board)
-        const newMovements = invertVerticalArray(movements)
-        return [newBoard,newMovements];
-    };
-    
-    const invertRowsCols = (board,movements) => {
-        let newBoard = invertRowColsArray(board);
-        let newMovements = invertRowColsArray(movements);
-        
-        return [newBoard,newMovements];
-    };
-
-    const invertHorizontalArray = (array) => {
-        return array.map((row) => {
-            return row.reverse().map((tile) => {
-                if (Array.isArray(tile)) {
-                    return [tile[0],-tile[1]];
-                }
-                return tile;
-            }
-            );
-        });
-    };
-
-    const invertVerticalArray = (array) => {
-        return array.reverse().map((row) => {
-            return row.map((tile) => {
-                if (Array.isArray(tile)) {
-                    return [-tile[0],tile[1]];
-                }
-                return tile;
-            });
-        });
-    };
-
-    const invertRowColsArray = (array) => {
-        const newArray = [];
-        for (let i = 0; i < array.length; i++) {
-            const newRow = [];
-            for (let j = 0; j < array.length; j++) {
-                let element = array[j][i];
-                if (Array.isArray(element)) {
-                    element = [element[1],-element[0]];
-                }
-                newRow.push(element);
-            }
-            newArray.push(newRow);
-        }
-        return newArray;
-    };
-
-    const rotateLeft = (board,movements) => {
-        [board,movements] = invertHorizontal(board,movements);
-        [board,movements] = invertRowsCols(board,movements);
-        return [board,movements];
-    };
-
-    const rotateRight = (board,movements) => {
-        [board,movements] = invertVertical(board,movements);
-        [board,movements] = invertRowsCols(board,movements);
-        return [board,movements];
-    };
-
-    const moveUp = (board,movements) => {
-        [board,movements] = rotateLeft(board,movements);
-        [board,movements] = moveLeft(board,movements);
-        return rotateRight(board,movements);
-    };
-
-    const moveDown = (board,movements) => {
-        [board,movements] = rotateRight(board,movements);
-        [board, movements] = moveLeft(board,movements);
-        return rotateLeft(board,movements);
-    };
-
-    const moveRight = (board,movements) => {
-        [board,movements] = invertHorizontal(board,movements);
-        [board, movements] = moveLeft(board,movements);
-        return invertHorizontal(board,movements);
-    };
-    
-    const moveLeft = (board,movements) => {
-        const newMovements = movements.map((row) => {
-            const newRow = row.map((tile) =>  tile);
-            return newRow;
-        });
-        const newBoard = board.map((row,rowNum) => {
-            const newRow = row.map((tile) =>  tile);
-            const merged = []; 
-            for (let i = 0; i < newRow.length ; i++) {
-                newMovements[rowNum][i] = [0,0];
-                if (newRow[i] !== 0) {
-                    let k = i;
-                    while (k > 0 && newRow[k-1] === 0) {
-                        k--;
-                    }
-                    
-                    if (newRow[k-1] === newRow[i] ) {
-                        if (merged.includes(k-1)) {
-                            newRow[k] = newRow[i];
-                            newRow[i] = 0;
-                            newMovements[rowNum][i] = [ 0,k-i];
-                            continue;
-                        }
-                        newRow[k-1] = newRow[i] * 2;
-                        newRow[i] = 0;
-                        newMovements[rowNum][i] = [0,k-i - 1];
-                        merged.push(k-1);
-                        continue;
-                    } 
-                    if (newRow[k] === 0) {
-                        newRow[k] = newRow[i];
-                        newRow[i] = 0;
-                        newMovements[rowNum][i] = [0,k-i];
-
-                    }
-                }
-
-            }
-            return newRow;
-        });
-        return [newBoard,newMovements];
-    };
-    
-    const getEmptyTiles = (board) => {
-        const emptyTiles = [];
-        for (let i = 0; i < board.length; i++) {
-            for (let j = 0; j < board.length; j++) {
-                if (board[i][j] === 0) {
-                    emptyTiles.push([i,j]);
-                }
-            }
-        }
-        return emptyTiles;
-    };
-
-    const createRandomTile = (board) => {
-        const emptyTiles = getEmptyTiles(board);
-        if (emptyTiles.length === 0) {
-            return board;
-        }
-        const randomTile = emptyTiles[Math.floor(Math.random() * emptyTiles.length)];
-        board[randomTile[0]][randomTile[1]] = Math.random() < 0.9 ? 2 : 4;
-        return board;
-    };
-    
+    // función que renderiza el tablero por filas
     const renderBoard = () => {
         const rows  = board.map((row, rowIndex) => {
-            let tiles= row.map((tile, colIndex) => {
-                return (
-                    <Tile
-                        value={tile}
-                        movement = {movements[rowIndex][colIndex]}
-                        key={rowIndex.toString() + colIndex.toString()}
-                    />
-                );
-            });
+            let tiles= row.map((tile, colIndex) => 
+                <Tile 
+                    value={tile} 
+                    key={rowIndex.toString() + colIndex.toString()}
+                />
+            );
             return (
                 <div className="board-row" key={rowIndex}>
                     {tiles}
@@ -263,7 +30,7 @@ const Board = ({action=null}) => {
             )
         });
         return (
-            <div className="board" onKeyDown={moveLeft}>
+            <div className="board">
                 {rows}
             </div>
         );
@@ -271,11 +38,10 @@ const Board = ({action=null}) => {
 
     
     return (
-    <div className="board-container">
-        {renderBoard()}
-        
-        
-        </div>);
+        <div className="board-container">
+            {renderBoard()}
+        </div>
+    );
 }
 
 export default Board;
--- src/components/Tile/Tile.jsx
+++ src/components/Tile/Tile.jsx
@@ -1,44 +1,12 @@
 import { useEffect, useState } from "react";
 import "./Tile.scss";
 
-const Tile = ({ value,  movement }) => {
-  const [tileValue, setTileValue] = useState(value);
-  const [bigger, setBigger] = useState(false);
-  const [moveTo, setmoveTo] = useState([0,0]);
-
-  useEffect(() => {
-    setTileValue(value);
-    if(value !==0){
-        setBigger(true);
-        setTimeout(() => {
-            setBigger(false);
-        }, 500); 
-    }
-    else{
-        setBigger(false);
-    }
-    }, [value]);
-  
-  useEffect(() => {
-    const moveY =movement?  movement[0]:0;
-    const moveX = movement ? movement[1] : 0;
-    if (moveX !== 0 || moveY !== 0) {
-        const toY= (moveY * 100).toString()+"%";
-        const toX= (moveX * 100).toString()+"%";
-        setmoveTo([toY,toX]);
-        
-    }
-  },[movement,value]);
-
-  const style =  {
-    '--toY': moveTo[0],
-    '--toX': moveTo[1],
-    };
-    const movingClass =   movement && (movement[0] !== 0 || movement[1] !== 0) ? "moving" : "";
+const Tile = ({ value }) => {
+  // estado tileValue que contiene el valor de la casilla, sacado del props value
+  const [tileValue, setTileValue] = useState(2);
   return (
     <div
-      className={`tile tile-${Math.min(2048,tileValue)} ${bigger && "bigger"} ${movingClass}`}
-      style={style}
+      className={`tile `}
     >
      {tileValue? tileValue: <span style={{visibility:"hidden"}}>{ tileValue}</span>}
     </div>
