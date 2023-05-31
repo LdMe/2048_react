9c7ed2e6d82fa651384876fcf4288b07a166d3e5
danel-torre-ubuntu <dlafuente003@gmail.com>
2023-05-31 11:52:29 +0200

    bigger animation

--- src/components/Tile/Tile.jsx
+++ src/components/Tile/Tile.jsx
@@ -2,10 +2,19 @@ import { useEffect, useState } from "react";
 import "./Tile.scss";
 
 const Tile = ({ value }) => {
-  
+  const [bigger, setBigger] = useState(false);
+  useEffect(() => {
+    if (value !== 0) {
+      setBigger(true);
+      setTimeout(() => {
+        setBigger(false);
+      }, 500);
+
+    }
+  }, [value]);
   return (
     <div
-      className={`tile tile-${Math.min(2048,value)}`}
+      className={`tile tile-${Math.min(2048,value)} ${bigger && "bigger"}`}
     >
      {value? value: <span style={{visibility:"hidden"}}>{ value}</span>}
     </div>
